bf1fca933e3d2a1fe8d40e77847dadb192cd798f
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-09-05 13:54:33 -0300

    Cria dependência do frontend com o modelo

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,18 +2,12 @@ import { Board } from "./game/board";
 import { Game } from "./game/game";
 import { Player } from "./player/player";
 import { GameResult } from "./result/game-result";
-import { HorizontalChecker } from "./result/horizontal-checker";
-import { VerticalChecker } from "./result/vertical-checker";
 import { Cell } from "./shared/cell";
 import { PlayerType } from "./shared/player-type";
-
-export { 
+ 
+export {
     Board,
-    Cell,
-    GameResult,
+    Cell, Game, GameResult,
     Player,
-    PlayerType,
-    HorizontalChecker, 
-    VerticalChecker,
-    Game
-}
+    PlayerType
+};
